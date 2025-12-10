ebeaa1c5d2adb495f519448d8c3d8bd15a3c5e15
pepegazxc <werewrs555@gmail.com>
2025-12-10 21:51:55 +0300

    fix(template) : Fixed a bug with package scanning

--- src/main/java/help/controller/Help.java
+++ src/main/java/help/controller/Help.java
@@ -1,7 +1,8 @@
-package controller;
+package help.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class Help {
